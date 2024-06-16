a8711602124fe26995757166815709bb3b2daae8
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-06-17 01:09:54 +0530

    Add patch

---
+++ GREEDY/330_Patching Array.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std; 
+class Solution 
+{
+    public:
+    int minPatches(vector<int>& nums, int n) {
+        long long maxReach =0;
+        int patch=0;
+        int i=0;
+        while(maxReach<n)
+        {
+            if(i<nums.size() && (nums[i]<=maxReach+1))
+            {
+                maxReach+=nums[i];
+                i++;
+            }
+            else
+            {
+                maxReach += (maxReach+1);
+                patch++;
+            }
+        }
+        return patch;
+        
+    }
+};
