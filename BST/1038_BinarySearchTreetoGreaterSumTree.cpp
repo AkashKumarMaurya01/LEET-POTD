4ec5573626b1d6d43e28a57fda26de8dd0ec04db
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-06-25 15:29:08 +0530

    greater BST

--- BST/1038_BinarySearchTreetoGreaterSumTree.cpp
+++ BST/1038_BinarySearchTreetoGreaterSumTree.cpp
@@ -1,37 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 
-void solve(TreeNode*root , int&sum)
+// just not show errror we ad this lines
+struct TreeNode
 {
-    if(root==NULL) return ;
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
-    solve(root->right , sum);
-    sum+=root->val;
+
+
+
+void solve(TreeNode *root, int &sum)
+{
+    if (root == NULL)
+        return;
+
+    solve(root->right, sum);
+    sum += root->val;
     root->val = sum;
-    solve(root->left , sum);
-
+    solve(root->left, sum);
 }
-class Solution {
+class Solution
+{
 public:
-    TreeNode* bstToGst(TreeNode* root) 
+    TreeNode *bstToGst(TreeNode *root)
     {
-        int sum=0;
-        solve(root, sum);  
-        return root;   
+        int sum = 0;
+        solve(root, sum);
+        return root;
     }
 };
 
-// approach : we have to just create a BST where we have to create a new bst where node value should be grater other
-//  we know that right element is always greter than left 
-//  we are just traverse right most then start updating the all the node value with sum of right most 
+//  approach : we have to just create a BST where we have to create a new bst where node value should be grater other
+//  we know that right element is always greter than left
+//  we are just traverse right most then start updating the all the node value with sum of right most
