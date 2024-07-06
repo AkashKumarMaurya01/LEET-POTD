936705f647f549fc583c9a35001c46b2f67c25ff
Akash kumar maurya <akashmaurya09994@gmail.com>
2024-07-06 23:05:44 +0530

    pass pillow

---
+++ Array/2582_PassthePillow.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int passThePillow(int n, int time)
+    {
+
+        int one_cycle_time = 2 * n - 2;
+        int pos = time % one_cycle_time;
+        return 1 + (pos >= n ? one_cycle_time - pos : pos);
+    }
+};
